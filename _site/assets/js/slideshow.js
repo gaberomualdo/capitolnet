2c266f1f8a524714a0b6ffb6d0adab4c9dd0a615
xtrp <36894883+xtrp@users.noreply.github.com>
2018-07-20 08:56:42 -0400

    bug fixes

--- _site/assets/js/slideshow.js
+++ _site/assets/js/slideshow.js
@@ -1,12 +1,17 @@
-$("div.featured_slideshow svg.featured_slideshow_back_button:not(.disabled)").on("click",function(){
-  _slideshow_slideshowMove(-1);
+$("div.featured_slideshow svg.featured_slideshow_back_button").on("click",function(){
+  if(!$(this).hasClass("disabled")){
+    _slideshow_slideshowMove(-1);
+  }
 });
-$("div.featured_slideshow svg.featured_slideshow_forward_button:not(.disabled)").on("click",function(){
-  _slideshow_slideshowMove(1);
+$("div.featured_slideshow svg.featured_slideshow_forward_button").on("click",function(){
+  if(!$(this).hasClass("disabled")){
+    _slideshow_slideshowMove(1);
+  }
 })
 function _slideshow_slideshowMove(moveNumber){
-  $('div.featured_slideshow').animate({scrollLeft: $(window).width()*moveNumber}, 600,'easeInOutExpo');
-  _slideshow_slideshowEvaluateDisabledButtons();
+  $('div.featured_slideshow').animate({scrollLeft: $('div.featured_slideshow').scrollLeft() + ($(window).width()*moveNumber)}, 600,'easeInOutExpo',function(){
+    _slideshow_slideshowEvaluateDisabledButtons();
+  });
 }
 function _slideshow_slideshowEvaluateDisabledButtons(){
   if($('div.featured_slideshow').scrollLeft() <= 0){
@@ -15,9 +20,9 @@ function _slideshow_slideshowEvaluateDisabledButtons(){
     $("div.featured_slideshow svg.featured_slideshow_back_button").removeClass("disabled");
   }
 
-  if(($('div.featured_slideshow')[0].scrollWidth - $('div.featured_slideshow').scrollLeft() + $(window).width()) <= 0){
-    $("div.featured_slideshow svg.featured_slideshow_back_button").addClass("disabled");
+  if($('div.featured_slideshow').scrollLeft() - $(window).width() == 0){
+    $("div.featured_slideshow svg.featured_slideshow_forward_button").addClass("disabled");
   }else{
-    $("div.featured_slideshow svg.featured_slideshow_back_button").removeClass("disabled");
+    $("div.featured_slideshow svg.featured_slideshow_forward_button").removeClass("disabled");
   }
 }
