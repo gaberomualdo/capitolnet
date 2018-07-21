c80436666718774e60e0012af0bcd235a9e551a0
xtrp <36894883+xtrp@users.noreply.github.com>
2018-07-21 11:12:37 -0400

    updated broken images

--- _site/assets/js/politician_api_functions.js
+++ _site/assets/js/politician_api_functions.js
@@ -52,6 +52,7 @@ function _politicians_requestsCompletedFunc(){
     }
 
     $("div.bio_container img").attr("src",politicianInfo.image);
+    $("div.bio_container img").attr("onerror","this.onerror = null;this.setAttribute('class','load_error')");
     $("div.bio_container div.bio_info h1").text(politicianInfo.name);
     updatePoliticianBio("Gender",politicianInfo.gender);
     updatePoliticianBio("Age",politicianInfo.age);
